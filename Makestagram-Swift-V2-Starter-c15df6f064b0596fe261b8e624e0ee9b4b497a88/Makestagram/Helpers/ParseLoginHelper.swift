d8bad2e257be5bd14d453d7b7a253ceb8f57a215
Robert Davis <robert@quarx.co>
2016-07-25 16:28:44 -0400

    instagram copy

---
+++ Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/Helpers/ParseLoginHelper.swift
@@ -0,0 +1,42 @@
+//
+//  ParseLoginHelper.swift
+//  Makestagram
+//
+//  Created by Benjamin Encz on 4/15/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import Foundation
+//import FBSDKCoreKit
+import Parse
+import ParseUI
+
+typealias ParseLoginHelperCallback = (PFUser?, NSError?) -> Void
+
+/** 
+  This class implements the 'PFLogInViewControllerDelegate' protocol. After a successfull login
+  it will call the callback function and provide a 'PFUser' object.
+*/
+
+
+class ParseLoginHelper : NSObject {
+  static let errorDomain = "com.makeschool.parseloginhelpererrordomain"
+  static let usernameNotFoundErrorCode = 1
+  static let usernameNotFoundLocalizedDescription = "Could not retrieve Facebook username"
+
+  let callback: ParseLoginHelperCallback
+  
+  init(callback: ParseLoginHelperCallback) {
+    self.callback = callback
+  }
+}
+
+extension ParseLoginHelper : PFLogInViewControllerDelegate {
+  
+  
+  func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+    // Determine if this is a Facebook login
+
+   }
+
+}
