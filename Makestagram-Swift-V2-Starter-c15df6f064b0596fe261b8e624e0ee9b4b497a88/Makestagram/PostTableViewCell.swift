b4af8b42236497736cbbde628e7475ee54ff9c38
Robert Davis <robert@quarx.co>
2016-08-01 14:16:08 -0400

    able to upload images but they are not loading to timeline

--- Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/PostTableViewCell.swift
+++ Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/PostTableViewCell.swift
@@ -6,11 +6,14 @@ import Parse
 
 class PostTableViewCell: UITableViewCell {
     
+    var postDisposable: DisposableType?
+    var likeDisposable: DisposableType?
+    
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var likesIconImageView: UIView!
-    @IBOutlet weak var moreButton: UIView!
-    @IBOutlet weak var likeButton: UIImageView!
     @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
     
     @IBAction func moreButtonTapped(sender: AnyObject) {
         
@@ -22,15 +25,42 @@ class PostTableViewCell: UITableViewCell {
     }
     
 
-    var post: Post! {
+    var post: Post? {
         didSet {
             // 1
+            postDisposable?.dispose()
+            likeDisposable?.dispose()
+            
             if let post = post {
-                //2
-                // bind the image of the post to the 'postImage' view
-                post.image.bindTo(postImageView.bnd_image)
+                // 2
+                postDisposable = post.image.bindTo(postImageView.bnd_image)
+                likeDisposable = post.likes.observe { (value: [PFUser]?) -> () in
+                    // 3
+                    if let value = value {
+                        // 4
+                        self.likesLabel.text = self.stringFromUserList(value)
+                        // 5
+                        self.likeButton.selected = value.contains(PFUser.currentUser()!)
+                        // 6
+                        self.likesIconImageView.hidden = (value.count == 0)
+                    } else {
+                        // 7
+                        self.likesLabel.text = ""
+                        self.likeButton.selected = false
+                        self.likesIconImageView.hidden = true
+                    }
+                }
             }
         }
+    }
+    
+    func stringFromUserList(userList: [PFUser]) -> String {
+        // 1
+        let usernameList = userList.map { user in user.username! }
+        // 2
+        let commaSeparatedUserList = usernameList.joinWithSeparator(", ")
+        
+        return commaSeparatedUserList
     }
     
     func downloadImage() {
--- Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/ViewControllers/TimeLineViewController.swift
+++ Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/ViewControllers/TimeLineViewController.swift
@@ -1,101 +1,28 @@
 import UIKit
 import Parse
 import Bond
+import ConvenienceKit
 
 var photoTakingHelper: PhotoTakingHelper?
 
-public class ParseHelper {
-    
-    
-    // Following Relation
-    static let ParseFollowClass       = "Follow"
-    static let ParseFollowFromUser    = "fromUser"
-    static let ParseFollowToUser      = "toUser"
-    
-    // Like Relation
-    static let ParseLikeClass         = "Like"
-    static let ParseLikeToPost        = "toPost"
-    static let ParseLikeFromUser      = "fromUser"
-    
-    // Post Relation
-    static let ParsePostUser          = "user"
-    static let ParsePostCreatedAt     = "createdAt"
-    
-    // Flagged Content Relation
-    static let ParseFlaggedContentClass    = "FlaggedContent"
-    static let ParseFlaggedContentFromUser = "fromUser"
-    static let ParseFlaggedContentToPost   = "toPost"
-    
-    // User Relation
-    static let ParseUserUsername      = "username"
-    
-    
-    // 2
-    static func timelineRequestForCurrentUser(completionBlock: PFQueryArrayResultBlock) {
-        let followingQuery = PFQuery(className: ParseFollowClass)
-        followingQuery.whereKey(ParseLikeFromUser, equalTo:PFUser.currentUser()!)
-        
-        let postsFromFollowedUsers = Post.query()
-        postsFromFollowedUsers!.whereKey(ParsePostUser, matchesKey: ParseFollowToUser, inQuery: followingQuery)
-        
-        let postsFromThisUser = Post.query()
-        postsFromThisUser!.whereKey(ParsePostUser, equalTo: PFUser.currentUser()!)
-        
-        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
-        query.includeKey(ParsePostUser)
-        query.orderByDescending(ParsePostCreatedAt)
-        
-        query.findObjectsInBackgroundWithBlock(completionBlock)
-    }
-    
-    static func likePost(user: PFUser, post: Post) {
-        let likeObject = PFObject(className: ParseLikeClass)
-        likeObject[ParseLikeFromUser] = user
-        likeObject[ParseLikeToPost] = post
-        
-        likeObject.saveInBackgroundWithBlock(nil)
-    }
-    
-    static func unlikePost(user: PFUser, post: Post) {
-        // 1
-        let query = PFQuery(className: ParseLikeClass)
-        query.whereKey(ParseLikeFromUser, equalTo: user)
-        query.whereKey(ParseLikeToPost, equalTo: post)
-        
-        query.findObjectsInBackgroundWithBlock { (results: [PFObject]?, error: NSError?) -> Void in
-            // 2
-            if let results = results as? [PFObject]? {
-                for likes in results! {
-                    likes.deleteInBackgroundWithBlock(nil)
-                }
-            }
-        }
-    }
-    
-    static func likesForPost(post: Post, completionBlock: PFQueryArrayResultBlock) {
-        let query = PFQuery(className: ParseLikeClass)
-        query.whereKey(ParseLikeToPost, equalTo: post)
-        // 2
-        query.includeKey(ParseLikeFromUser)
-        
-        query.findObjectsInBackgroundWithBlock(completionBlock)
-    }
-}
 
-class TimeLineViewController: UIViewController {
+
+class TimeLineViewController: UIViewController, TimelineComponentTarget {
     
+    
+    var timelineComponent: TimelineComponent<Post, TimeLineViewController>!
     var posts: [Post] = []
     
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        timelineComponent = TimelineComponent(target: self)
         self.tabBarController?.delegate = self
-        
-        
-            }
-
-        func takePhoto() {
+    }
+    
+    func takePhoto() {
             // instantiate photo taking class, provide callback for when photo is selected
             photoTakingHelper =
                 PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
@@ -106,20 +33,26 @@ class TimeLineViewController: UIViewController {
             }
         }
     
+    let defaultRange = 0...4
+    let additionalRangeSize = 5
+    
+    func loadInRange(range: Range<Int>, completionBlock: ([Post]?) -> Void) {
+        // 1
+        ParseHelper.timelineRequestForCurrentUser(range) {
+            (result: [PFObject]?, error: NSError?) -> Void in
+            // 2
+            let posts = result as? [Post] ?? []
+            // 3
+            completionBlock(posts)
+        }
+    }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        ParseHelper.timelineRequestForCurrentUser{
-            (result: [PFObject]?, error: NSError?) -> Void in
-            
-            self.posts = result as? [Post] ?? []
-            
-            self.tableView.reloadData()
-        }
-    }    
+        timelineComponent.loadInitialIfRequired()
+    }
     
-
 }
 
 
@@ -146,21 +79,28 @@ extension TimeLineViewController: UITabBarControllerDelegate {
 extension TimeLineViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // 1
-        return posts.count
+        return timelineComponent.content.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        
+        let post = timelineComponent.content[indexPath.row]
+        cell.downloadImage()
+        post.fetchLikes()
+        cell.post = post
+        
+        return cell
+    }
+}
 
-            let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
-            
-            let post = posts[indexPath.row]
-            cell.downloadImage()
-            post.fetchLikes()
-            cell.post = post
-            
-            return cell
-        }
+extension TimeLineViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        timelineComponent.targetWillDisplayEntry(indexPath.row)
+    }
+    
 }
 
 
