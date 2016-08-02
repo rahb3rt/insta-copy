9457117b49a1e7f25b616d7db178df150c1f4a6e
Robert Davis <robert@quarx.co>
2016-08-01 22:21:27 -0400

    added search bar and some of its functions

--- Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/ViewControllers/FriendSearchViewController.swift
+++ Makestagram-Swift-V2-Starter-c15df6f064b0596fe261b8e624e0ee9b4b497a88/Makestagram/ViewControllers/FriendSearchViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class FriendSearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
